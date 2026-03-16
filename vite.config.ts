749cd8bbd505229f3a83ed1009f20ca55762de20
Quochau151 <tranlequochau.blc@gmail.com>
2026-03-17 00:43:01 +0700

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
-import path from "path";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 3000,
+    port: 8080,
     hmr: {
       overlay: false,
     },
